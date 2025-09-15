7eb45b7f518c7da2649efd0f06661f8b1ae64ca0
Slivka <max04032007@gmail.com>
2025-09-15 10:31:12 +0300

    lab 2 completed

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -1,27 +1,100 @@
+
 package ua.opnu;
 
+/**
+ * Клас BankAccount описує банківський рахунок клієнта.
+ */
 public class BankAccount {
-    String name;
-    double balance;
-    double transactionFee;
 
-    void deposit(double amount) {
-        // TODO: modify method body
-        balance = balance + amount;
+    /**
+     * Ім'я власника рахунку.
+     */
+    private String name;
+
+    /**
+     * Поточний баланс рахунку.
+     */
+     double balance;
+
+    /**
+     * Комісія за транзакцію.
+     */
+     double transactionFee = 0.0;
+
+    /**
+     * Метод для поповнення балансу.
+     *
+     * @param amount Сума для поповнення. Повинна бути більшою за нуль.
+     */
+    public void deposit(final double amount) {
+        if (amount > 0) {
+            this.balance = this.balance + amount;
+        }
     }
 
-    double getBalance() {
+    /**
+     * Повертає поточний баланс рахунку.
+     *
+     * @return Поточний баланс.
+     */
+    public double getBalance() {
         return this.balance;
     }
 
-    boolean withdraw(double amount) {
-        // TODO: modify method body
-        balance = balance - amount;
-        return true;
+    /**
+     * Метод для зняття грошей з рахунку.
+     *
+     * @param amount Сума для зняття. Повинна бути більшою за нуль.
+     * @return true, якщо операція успішна, інакше false.
+     */
+    public boolean withdraw(final double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
+        double totalAmount = amount + this.transactionFee;
+
+        if (this.balance >= totalAmount) {
+            this.balance = this.balance - totalAmount;
+            return true;
+        }
+
+        return false;
     }
 
-    boolean transfer(BankAccount receiver, double amount) {
-        // TODO: modify method body
+    /**
+     * Метод для переказу грошей на інший рахунок.
+     *
+     * @param receiver Рахунок-одержувач.
+     * @param amount   Сума для переказу. Повинна бути більшою за нуль.
+     * @return true, якщо операція успішна, інакше false.
+     */
+    public boolean transfer(final BankAccount receiver, final double amount) {
+        if (amount <= 0) {
+            return false;
+        }
+
+        double totalAmountToWithdraw = amount + this.transactionFee;
+
+        if (this.balance >= totalAmountToWithdraw) {
+            // Використовуємо існуючий метод withdraw,
+            // щоб уникнути дублювання коду
+            if (this.withdraw(amount)) {
+                receiver.deposit(amount);
+                return true;
+            }
+        }
         return false;
+    }
+
+    // --- Додані методи доступу (геттери та сеттери) ---
+
+    /**
+     * Повертає ім'я власника рахунку.
+     *
+     * @return a {@link java.lang.String} object.
+     */
+    public String getName() {
+        return name;
     }
 }
